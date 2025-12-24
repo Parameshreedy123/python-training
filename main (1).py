64f600f8efd00d2eb55aafddd3adb26778b1a2dd
parameshreddy <parameshreddygumma3@gmail.com>
2025-12-24 11:23:37 +0530

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -18,3 +18,12 @@ for i in range(x):
     for j in range(i+1):
         print(j+1,end=" ")
     print("\n")
+
+#pattern of pyramidal 
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
