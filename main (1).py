609c412faafa0aee0cd1740c8ddda9e7cf4b6974
parameshreddy <parameshreddygumma3@gmail.com>
2025-12-24 12:22:29 +0530

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -77,6 +77,15 @@ for i in range(n):
            print(" ",end=" ")
     print()
 
+#pattern of hallow traiangle
+n=int(input())
+for i in range(n):
+    for j in range(i+1):
+        if i==n-1 or j==0 or i ==j:
+           print("*",end=" ")
+        else:
+           print(" ",end=" ")
+    print()
 
 
 
