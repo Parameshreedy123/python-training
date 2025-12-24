54ebefe8aa2647a48aca106922ba914cfd999366
parameshreddy <parameshreddygumma3@gmail.com>
2025-12-24 11:30:41 +0530

    Implement diamond pattern printing
    Added code to print a diamond pattern based on user input.

--- main (1).py
+++ main (1).py
@@ -27,3 +27,19 @@ for i in range(x):
     for j in range(i+1):
         print("*",end=" ")
     print()
+
+#pattern of diamond
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(x-2,-1,-1):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
